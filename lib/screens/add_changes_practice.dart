b2f2082a0848e2d99d485e3f609f63cbc857f72a
Your Name <your.email@example.com>
2024-12-12 09:06:58 +0500

    add chnages screen

---
+++ lib/screens/add_changes_practice.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AddChangesPractice extends StatelessWidget {
+  const AddChangesPractice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.amber,
+     child: Container(),
+    );
+  }
+}
